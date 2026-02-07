89ff63f101fe33373f4873dd25f0db3133f2eda2
David Crespo <david.crespo@correounivalle.edu.co>
2026-02-07 10:25:41 -0500

    added keymap for pushing in Git status

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -7,6 +7,7 @@ return {
 			pattern = "fugitive",
 			callback = function()
 				vim.keymap.set("n", "A", ":Git add -A<CR>", { buffer = true, desc = "Stage all" })
+				vim.keymap.set("n", "P", ":Git push <CR>", { buffer = true, desc = "Push" })
 			end,
 		})
 	end,
