0c811aab3bc0df98bbdeaef5c53829fd614ffb25
Quimisagi <quimisagi@gmail.com>
2026-05-03 01:34:01 +0900

    Disabled updates checker notification

--- init.lua
+++ init.lua
@@ -34,7 +34,7 @@ require("lazy").setup({
   },
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
 
 vim.api.nvim_create_autocmd("UIEnter", {
