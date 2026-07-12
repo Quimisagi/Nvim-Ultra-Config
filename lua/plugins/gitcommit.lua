cd8a31c6b3b43b43002613e322a6e4452fabb1c6
Quimisagi <quimisagi@gmail.com>
2026-07-12 13:12:59 +0900

    updated git commit generation model and ollama URL

--- lua/plugins/gitcommit.lua
+++ lua/plugins/gitcommit.lua
@@ -7,7 +7,7 @@ return {
 		cmd = "GitCommitGenerate",
 		config = function()
 			require("gitcommit").setup({
-				model = "qwen2.5-coder",
+				model = "qwen2.5-coder:3b",
 				ollama_url = "http://localhost:11434/api/generate",
 				max_diff_chars = 12000,
 				recent_commits_count = 15,
